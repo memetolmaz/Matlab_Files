f5966a12f98b7d01b9d0c8690ecfccbd36afd1a0
memetolmaz <83218999+memetolmaz@users.noreply.github.com>
2022-12-15 14:17:07 +0300

    Add files via upload

---
+++ perfect_number.m
@@ -0,0 +1,49 @@
+clc;
+clear;
+%Perfect number = In number theory , The sum of all positive divisors other than itself is a number equal to itself.
+sayi = input('sayý = ');
+liste = [];
+
+
+for i=1:sayi
+    if ( mod(sayi,i) == 0 )
+        liste = [liste,i];
+    end
+end
+
+if (sum(liste) == sayi*2)
+    fprintf('%d sayýsý mükemmel sayýdýr. \n',sayi);
+    
+else
+    fprintf('%d sayýsý mükemmel sayý deðildir \n',sayi);
+end
+
+
+disp(liste)
+% In mathematics ,formula for finding the perfect number = 2p?1(2p?1)
+
+%%
+
+% Lets find fourth perfect number :
+liste = [];
+mliste= [];
+sayi =1;
+while (1)
+liste = [];
+for i=1:sayi
+    if ( mod(sayi,i) == 0 )
+        liste = [liste,i];
+    end
+end
+
+if (sum(liste) == sayi*2)
+    mliste = [mliste,sayi];
+end
+if (length(mliste) == 4)
+    break
+end
+sayi = sayi + 1;
+end
+disp(mliste(4))
+
+% The fourth perfect number is 8128
